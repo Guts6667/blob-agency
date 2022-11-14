469913aba457220457048bcdb119f88719f3128f
Rayan <rayanchambet1@gmail.com>
2022-11-14 13:14:47 +0100

    V.1.4
    Added AC-Ventures Card
    Corrected typo

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <section className="container container__home" id='home'>
       <h1>Bring your <strong>dream</strong> to life, take your project to the <strong>next level.</strong></h1>
-      <p>BLOB Agency designs softwares and websites. Human interactions, accessibility, environment and reliaiblity are the key values driving our works.</p>
+      <p>BLOB Agency designs softwares and websites. Human interactions, accessibility, environment and reliability are the key values driving our works.</p>
       <button className='btn'><a href="#contact">Contact</a></button>
     </section>
   );
